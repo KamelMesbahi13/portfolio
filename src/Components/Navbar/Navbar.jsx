49aa9322317c6e04862c6a45b33212f708093115
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2025-10-29 17:05:03 +0100

    adding nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import Logo from "../../assets/kmLogo.png";
 
 const Navbar = () => {
   return (
     <div className="container">
-      <div className="w-12 h-12">
-        <img src={Logo} alt="Logo" />
+      <div>
+        <img src={Logo} alt="Logo" className="w-12 h-12" />
       </div>
     </div>
   );
