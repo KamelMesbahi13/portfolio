46f8a2eb1d898c2c33d7aca4b8851fd9c026d689
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2025-10-29 17:50:53 +0100

    adding nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,20 @@ import Logo from "../../assets/kmLogo.png";
 const Navbar = () => {
   return (
     <div className="container">
-      <div>
-        <img src={Logo} alt="Logo" className="w-12 h-12" />
+      <div className="flex items-center justify-between ">
+        <div>
+          <div className="flex items-center ">
+            <div>
+              <img src={Logo} alt="Logo" className="w-16" />
+            </div>
+            <div>
+              <p>Web Developer / Web Designer </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-5xl">ff</h2>
+        </div>
       </div>
     </div>
   );
