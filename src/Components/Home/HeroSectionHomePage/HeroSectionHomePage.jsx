52b00fdfb8ae0244ceee230274fadb873b2b1f90
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2025-10-30 19:36:48 +0100

    adding the background of the home page

--- src/Components/Home/HeroSectionHomePage/HeroSectionHomePage.jsx
+++ src/Components/Home/HeroSectionHomePage/HeroSectionHomePage.jsx
@@ -1,5 +1,28 @@
 const HeroSectionHomePage = () => {
-  return <div className="backone relative top-[-5rem] -z-10"></div>;
+  return (
+    <div className="backone relative top-[-5rem] -z-10">
+      <div className="container text-white">
+        <div className="absolute -translate-y-1/2 top-1/2">
+          <div>
+            <div>
+              <h1>
+                Hi, my name <br /> is{" "}
+                <span className="font-semibold text-secondColor">
+                  Mesbahi Kamel
+                </span>{" "}
+                .
+              </h1>
+              <div>
+                <p>
+                  I'm an independent creative developer from Algiers, Algeria.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSectionHomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
 export const theme = {
+  colors: {
+    // mainColor: "#0a6bff",
+    secondColor: "#F87B1B",
+    white: "#ffffff",
+    red: "#ff0f0f",
+  },
   extend: {},
 };
 export const plugins = [];
