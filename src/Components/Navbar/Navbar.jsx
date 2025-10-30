9987af1a0f0c95dbaf0e6db2f7e907f0681ada6c
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2025-10-30 18:34:17 +0100

    adding nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,28 +1,30 @@
-import Logo from "../../assets/kmLogo.png";
+import Logo from "../../assets/kmLogowhite.png";
 
 const Navbar = () => {
   return (
-    <div className="container">
-      <div className="flex items-center justify-between border-b border-black">
-        <div>
-          <div className="flex items-center ">
+    <div className="backone">
+      <div className="container">
+        <div className="flex items-center justify-between text-white border-b border-white ">
+          <div>
+            <div className="flex items-center ">
+              <div>
+                <img src={Logo} alt="Logo" className="w-16" />
+              </div>
+              <div className="ml-6 font-medium">
+                <h6>Web Developer / Web Designer </h6>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between">
             <div>
-              <img src={Logo} alt="Logo" className="w-16" />
+              <p className="text-sm">Works</p>
             </div>
-            <div className="ml-6 font-medium">
-              <h6>Web Developer / Web Designer </h6>
+            <div className="mx-2">
+              <p className="text-sm">About</p>
             </div>
-          </div>
-        </div>
-        <div className="flex justify-between">
-          <div>
-            <p className="text-sm">Works</p>
-          </div>
-          <div className="mx-2">
-            <p className="text-sm">About</p>
-          </div>
-          <div>
-            <p className="text-sm">Contact</p>
+            <div>
+              <p className="text-sm">Contact</p>
+            </div>
           </div>
         </div>
       </div>
