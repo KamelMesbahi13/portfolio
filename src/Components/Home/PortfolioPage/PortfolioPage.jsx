9930e314b2b859e6990120c068f8c191fcc834e8
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2025-11-09 14:36:27 +0100

    adding sections to the home page

--- src/Components/Home/PortfolioPage/PortfolioPage.jsx
+++ src/Components/Home/PortfolioPage/PortfolioPage.jsx
@@ -3,18 +3,14 @@ import HeroFirstContent from "../HeroFirstContent/HeroFirstContent";
 
 const PortfolioPage = () => {
   return (
-    <div className="min-h-screen text-white">
-      {/* Main Content */}
-      <div className="pt-24 px-6 max-w-[1400px] mx-auto">
+    <div className="container">
+      <div className="pt-24 mx-auto">
         <div className="flex flex-col items-start gap-8 lg:flex-row lg:gap-16">
-          {/* Left Side - Sticky Card */}
           <div className="flex-shrink-0 w-full lg:w-80">
             <Card />
           </div>
 
-          {/* Right Side - Scrollable Content */}
           <div className="flex-1 space-y-16">
-            {/* Hero Section */}
             <HeroFirstContent />
           </div>
         </div>
