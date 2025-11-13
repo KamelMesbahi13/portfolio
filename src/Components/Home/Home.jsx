ceb71427c99a14722e4527e1665e817085aada43
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2025-11-13 18:36:35 +0100

    adding featured work section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,14 @@
-// eslint-disable-next-line no-unused-vars
-import { motion } from "framer-motion";
 import PortfolioPage from "./PortfolioPage/PortfolioPage";
 import RecentWork from "./RecentWork/RecentWork";
 import MyQualityServices from "./MyQualityServices/MyQualityServices";
+import Technologies from "./Technologies/Technologies";
 
 const Home = () => (
   <div>
-    <motion.div
-      initial={{ opacity: 0, y: 0 }}
-      animate={{ opacity: 1, y: 10 }}
-      exit={{ opacity: 0, y: 0 }}
-      transition={{ duration: 0.8, ease: "easeOut" }}
-    >
-      <PortfolioPage />
-      <MyQualityServices />
-    </motion.div>
+    <PortfolioPage />
+    <MyQualityServices />
     <RecentWork />
+    <Technologies />
   </div>
 );
 
---
+++ src/Components/Home/Technologies/Technologies.jsx
@@ -0,0 +1,9 @@
+const Technologies = () => {
+  return (
+    <div className="container">
+      <div>Lorem, ipsum.</div>
+    </div>
+  );
+};
+
+export default Technologies;
