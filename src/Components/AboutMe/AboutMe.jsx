b49b19c6f4af647a474ed661fd4a6121762e8349
Kamel Mesbahi <elkamel.mesbahi@gmail.com>
2026-01-03 00:35:47 +0100

    adding what we offer

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,11 +1,13 @@
 import AboutUsHome from "./AboutMeHome/AboutMeHome";
 import AboutMeIntroduction from "./AboutMeIntroduction/AboutMeIntroduction";
+import WhatWeOffer from "./WhatWeOffer/WhatWeOffer";
 
 const AboutUs = () => {
   return (
     <div>
       <AboutUsHome />
       <AboutMeIntroduction />
+      <WhatWeOffer />
     </div>
   );
 };
